f993031bb504ac956ddfe61350962aa9d2e4f065
HChong <hanchong@souche.com>
2018-01-26 10:37:17 +0800

    first commit
    Signed-off-by: HChong <hanchong@souche.com>

---
+++ HCKVO/HCKVO/NSObject+KVO.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+KVO.m
+//  HCKVO
+//
+//  Created by HChong on 2018/1/26.
+//  Copyright © 2018年 HChong. All rights reserved.
+//
+
+#import "NSObject+KVO.h"
+
+@implementation NSObject (KVO)
+
+@end
